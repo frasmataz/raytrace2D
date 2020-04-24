222880253dbec86b349a67120c1e12b0118e2cfb
Fraser Sharp <fraserrsharp@gmail.com>
2020-04-24 19:14:19 +0100

    refactor: Clean up line segment intersection code

--- raytrace.py
+++ raytrace.py
@@ -65,8 +65,6 @@ class Game:
             numpy.add(self.playerPosition, self.playerVelocity))
         self.mousePosition = pygame.mouse.get_pos()
 
-    # Given three colinear points p, q, r, the function checks if
-    # point q lies on line segment 'pr'
     def onSegment(self, p, q, r):
         if ((q[0] <= max(p[0], r[0])) and (q[0] >= min(p[0], r[0])) and
                 (q[1] <= max(p[1], r[1])) and (q[1] >= min(p[1], r[1]))):
@@ -74,81 +72,42 @@ class Game:
         return False
 
     def orientation(self, p, q, r):
-        # to find the orientation of an ordered triplet (p,q,r)
-        # function returns the following values:
-        # 0 : Colinear points
-        # 1 : Clockwise points
-        # 2 : Counterclockwise
-
-        # See https://www.geeksforgeeks.org/orientation-3-ordered-points/amp/
-        # for details of below formula.
-
         val = (float(q[1] - p[1]) * (r[0] - q[0])) - \
             (float(q[0] - p[0]) * (r[1] - q[1]))
         if (val > 0):
-
-            # Clockwise orientation
             return 1
         elif (val < 0):
-
-            # Counterclockwise orientation
             return 2
         else:
-
-            # Colinear orientation
             return 0
 
-    # The main function that returns true if
-    # the line segment 'p1q1' and 'p2q2' intersect.
-    def doIntersect(self, p1, q1, p2, q2):
-
-        # Find the 4 orientations required for
-        # the general and special cases
+    def getIntersectPoint(self, p1, q1, p2, q2):
         o1 = self.orientation(p1, q1, p2)
         o2 = self.orientation(p1, q1, q2)
         o3 = self.orientation(p2, q2, p1)
         o4 = self.orientation(p2, q2, q1)
 
-        # General case
-        if ((o1 != o2) and (o3 != o4)):
-            return True
+        if (((o1 != o2) and (o3 != o4))
+        or ((o1 == 0) and self.onSegment(p1, p2, q1))
+        or ((o2 == 0) and self.onSegment(p1, q2, q1))
+        or ((o3 == 0) and self.onSegment(p2, p1, q2))
+        or ((o4 == 0) and self.onSegment(p2, q1, q2))):
+            xdiff = (p1[0] - q1[0], p2[0] - q2[0])
+            ydiff = (p1[1] - q1[1], p2[1] - q2[1])
 
-        # Special Cases
+            def det(a, b):
+                return a[0] * b[1] - a[1] * b[0]
 
-        # p1 , q1 and p2 are colinear and p2 lies on segment p1q1
-        if ((o1 == 0) and self.onSegment(p1, p2, q1)):
-            return True
+            div = det(xdiff, ydiff)
+            if div == 0:
+                return None
 
-        # p1 , q1 and q2 are colinear and q2 lies on segment p1q1
-        if ((o2 == 0) and self.onSegment(p1, q2, q1)):
-            return True
+            d = (det(*(p1, q1))), det(*(p2, q2))
+            x = det(d, xdiff) / div
+            y = det(d, ydiff) / div
+            return x, y
 
-        # p2 , q2 and p1 are colinear and p1 lies on segment p2q2
-        if ((o3 == 0) and self.onSegment(p2, p1, q2)):
-            return True
-
-        # p2 , q2 and q1 are colinear and q1 lies on segment p2q2
-        if ((o4 == 0) and self.onSegment(p2, q1, q2)):
-            return True
-
-        # If none of the cases
-        return False
-
-    def getIntersectPoint(self, line1, line2):
-        xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-        ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
-
-        def det(a, b):
-            return a[0] * b[1] - a[1] * b[0]
-
-        div = det(xdiff, ydiff)
-        if div == 0:
-            return None
-
-        d = (det(*line1), det(*line2))
-        x = det(d, xdiff) / div
-        y = det(d, ydiff) / div
-        return x, y
+        return None
 
     def lineLength(self, p1, p2):
         return numpy.sqrt(numpy.square(p1[0] - p2[0]) + numpy.square(p1[1] - p2[1]))
@@ -164,10 +123,9 @@ class Game:
         minIntersectPos = (0, 0)
 
         for surface in self.surfaces:
-            if self.doIntersect(self.playerPosition, endPos, surface[0], surface[1]):
-                intersectPos = self.getIntersectPoint(
-                    (self.playerPosition, endPos), surface)
-            else:
+            intersectPos = self.getIntersectPoint( self.playerPosition, endPos, surface[0], surface[1] )
+
+            if intersectPos == None:
                 continue
 
             length = self.lineLength(self.playerPosition, intersectPos)
